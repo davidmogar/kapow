3dc9fb942c21e378d4a1db06274512837102366d
David Moreno Garcia <david.mogar@cern.ch>
2018-02-03 17:01:59 +0100

    Use noninteractive with fish

--- modules/fish/install.sh
+++ modules/fish/install.sh
@@ -6,4 +6,5 @@ chsh -s $(which fish)
 # Install Oh My Fish
 # https://github.com/oh-my-fish/oh-my-fish
 #
-curl -L https://get.oh-my.fish | fish
+curl -L https://get.oh-my.fish > install
+fish install -y --noninteractive
