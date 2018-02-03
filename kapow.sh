6c66b3cb78a8c1976e369eb32eedb8afb6728311
David Moreno Garcia <david.mogar@cern.ch>
2018-02-03 12:03:52 +0100

    Initial commit

---
+++ kapow.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+readonly MODULES_PATH = './modules'
+
+function configure_repositories {
+    for directory in $MODULES_PATH
+    do
+        file="$directory/repository.sh"
+        if [ ! -f $file]
+        then
+            source $file
+        fi
+    done
+}
+
+function install_modules {
+    for directory in $MODULES_PATH
+    do
+        source $directory/install.sh
+    done
+}
+
+function update_system {
+    sudo apt -y update
+    sudo apt -y upgrade
+}
+
+configure_repositories
+update_system
+install_modules
